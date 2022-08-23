5a3a5292a5e9cf5a05af894057c66026bbd3e45d
Muskan <90378057+muskan2901@users.noreply.github.com>
2022-08-23 18:05:08 +0530

    Add files via upload

---
+++ routes/userRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const userController = require('../controller/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.get('/info', authController.protect, authController.info);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.all('*', (req, res, next) =>
+  res.status(404).json({msg : 'Route Not Found'})
+);
+
+module.exports = router;
